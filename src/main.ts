4be8f339952e3fe89cd7ab79127fb1017c76b4ca
joseph <josephtsegen10@gmail.com>
2023-03-24 01:33:07 +0100

    done with log and log rotation

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,56 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerInit } from './swagger';
+import * as winston from 'winston';
+import * as DailyRotateFile from 'winston-daily-rotate-file';
+import {
+  utilities as nestWinstonModuleUtilities,
+  WinstonModule,
+} from 'nest-winston';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const infoLogRotationTransport = new DailyRotateFile({
+    filename: './/logs//info',
+    datePattern: 'YYYY-MM-DD-HH:MM',
+    zippedArchive: true,
+    maxSize: '10m',
+    maxFiles: '80d',
+    level: 'info',
+    extension: '.log',
+  });
+
+  const errorLogRotationTransport = new DailyRotateFile({
+    filename: './/logs//error',
+    datePattern: 'YYYY-MM-DD-HH:MM',
+    zippedArchive: true,
+    maxSize: '10m',
+    maxFiles: '80d',
+    level: 'error',
+    extension: '.log',
+  });
+
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    logger: WinstonModule.createLogger({
+      level: 'info',
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.ms(),
+        winston.format.prettyPrint(),
+      ),
+      transports: [
+        infoLogRotationTransport,
+        errorLogRotationTransport,
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            winston.format.ms(),
+            nestWinstonModuleUtilities.format.nestLike('app'),
+          ),
+        }),
+      ],
+    }),
+  });
 
   SwaggerInit(app);
 
